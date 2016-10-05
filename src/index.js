3297f9dadee31381377563c0c2a0695e854fd9db
lugithub <lu.bin.lu.bin@gmail.com>
2016-10-04 23:01:23 -0400

    rest properties, spread properties

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import Counter from './components/Counter';
 //to an identifier, counter.
 import counter from './reducers';
 
+// Rest properties
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // { a: 3, b: 4 }
+
+// Spread properties
+let n = { x, y, ...z };
+console.log(n); // { x: 1, y: 2, a: 3, b: 4 }
+
 const createStore = reducer => {
   let state;
 
