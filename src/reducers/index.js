b788c50b5137831df72dc0d45c9a340698b7a626
lugithub <lu.bin.lu.bin@gmail.com>
2016-10-08 11:22:10 -0400

    functional programming, implement combineReducers from scratch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,35 @@
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
-import {combineReducers} from 'redux';
 
 //todos key is the key of the corresponding field in the state
 //todos value is the reducer
 //responsible to update the corresponding field in the state
 export default combineReducers({todos, visibilityFilter});
+
+//functional programming: takes functions and returns a function
+function combineReducers(reducers) {
+
+  //it is still a pure function
+  return (state = {}, action) => {
+
+    const keys = Object.keys(reducers);
+
+    // return keys.reduce((previouseState, key) => ({
+    //     ...previouseState,
+    //
+    //     //computed property name
+    //     [key]: reducers[key](state[key], action)
+    //   }),
+    //   state
+    // );
+
+    return keys.reduce((previouseState, key) => {
+        previouseState[key] = reducers[key](state[key], action);
+        return previouseState;
+      },
+      {} //initial value is mutable
+    );
+
+  };
+
+}
