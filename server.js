68b5af115099290445cef338b6352f8bfe5fa122
bin lu <lu.bin.lu.bin@gmail.com>
2016-10-24 11:56:50 -0400

    react-router, express serving default page

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+app.get('/', function(req, res) {
+  res.sendFile('hello.html', { root: path.join(__dirname, 'public')} );  
+});
+
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
--- src/root.js
+++ src/root.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
 //component has uppercase naming convention
 import ToDoApp from './components/ToDoApp';
 
 const Root = ({store}) =>
   <Provider store={store}>
-    <ToDoApp/>
+    <Router history={browserHistory} >
+      <Route path='/' component={ToDoApp}/>
+    </Router>
   </Provider>;
 
 export default Root;
