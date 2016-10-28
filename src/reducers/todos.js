21e4c57f73f2d0a31c00abdaf9251aa973cb7b32
lugithub <lu.bin.lu.bin@gmail.com>
2016-10-27 23:24:57 -0400

    normalize state shape from array to lookup map i.e. object

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,22 +1,20 @@
+import {combineReducers} from 'redux';
 import todo from './todo';
 import _ from 'lodash';
 
 //prefer reducers to the persistedState to specify the default
 //because that makes it easy to test the changes
-const todos = (state = [], action) => {
+const byId = (state = {}, action) => {
   const {id} = action;
   const index = _.findIndex(state, {id});
 
   switch (action.type) {
     case 'ADD_TODO':
-      return [...state, todo(undefined, action)];
-
     case 'TOGGLE_TODO':
-      return [
-        ...state.slice(0, index),
-        todo(state[index], action),
-        ...state.slice(index+1)
-      ];
+      return {
+        ...state,
+        [action.id]: todo(state[action.id], action)
+      };
 
     default:
       //could return the initial state
@@ -24,18 +22,34 @@ const todos = (state = [], action) => {
   }
 };
 
+//this is implementation details of converting the lookup map to array
+//my approarch of lodash doesn't need it
+const allIds = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [...state, action.id];
+    default:
+      return state;
+  }
+};
+
+const todos = combineReducers({
+  byId,
+  allIds
+});
+
 export default todos;
 
 //selector
 export const getVisibleToDos = (state, filter) => {
-  return state.filter(todo => {
-    switch (filter) {
-      case 'all':
-        return true;
-      case 'completed':
-        return todo.completed;
-      default:
-        return !todo.completed;
-    }
-  });
+  const {byId} = state;
+
+  switch (filter) {
+    case 'all':
+      return _.map(byId);
+    case 'completed':
+      return _.filter(byId, {completed: true});
+    default:
+      return _.filter(byId, {completed: false});
+  }
 };
