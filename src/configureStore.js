4c63d61e379eb65fcf43dc06ab19618991c400a9
lugithub <lu.bin.lu.bin@gmail.com>
2016-10-29 22:49:52 -0400

    wrap dispatch to log actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -5,10 +5,33 @@ import throttle from 'lodash/throttle';
 //reducer has lowercase naming convertion
 import todoApp from './reducers';
 
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return action => {
+    console.group('%cdispatching action: ', 'color: blue', action);
+    console.log('%cprevious state: ', 'color: gray', store.getState());
+
+    //i had an error using store.dispatch instead of rawDispatch, which
+    //calls the wrapper recursively
+    const value = rawDispatch(action);
+    console.log('%ccurrent state: ', 'color: green', store.getState());
+    console.groupEnd();
+    return value;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
-  
+
   const store = createStore(todoApp, persistedState);
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(throttle(() => {
     saveState({
--- src/index.js
+++ src/index.js
@@ -5,15 +5,6 @@ import Root from './root';
 
 const store = configureStore();
 
-const rawDispatch = store.dispatch;
-store.dispatch = action => {
-  console.group('%cdispatching action: ', 'color: green', action);
-  console.log('%cprevious state: ', 'color: green', store.getState());
-  rawDispatch(action);
-  console.log('%ccurrent state: ', 'color: green', store.getState());
-  console.groupEnd();
-};
-
 //because child container components subscribe to the store themselfvs
 ReactDOM.render(
   <Root store={store}/>,
