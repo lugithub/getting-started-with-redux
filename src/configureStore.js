f6d92bcc06d970436fd27bbb79caa81bd3e1dbf6
lugithub <lu.bin.lu.bin@gmail.com>
2016-11-03 23:14:39 -0400

    middleware chain

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,48 +4,51 @@ import throttle from 'lodash/throttle';
 //reducer has lowercase naming convertion
 import todoApp from './reducers';
 
-const addLoggingToDispatch = store => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-    return rawDispatch;
-  }
+//currying
+const logger = store => next => {
+    if (!console.group) {
+      return next;
+    }
 
-  return action => {
-    console.group('%cdispatching action: ', 'color: blue', action);
-    console.log('%cprevious state: ', 'color: gray', store.getState());
+    return action => {
+      console.group('%cdispatching action: ', 'color: blue', action);
+      console.log('%cprevious state: ', 'color: gray', store.getState());
 
-    //i had an error using store.dispatch instead of rawDispatch, which
-    //calls the wrapper recursively
-    const value = rawDispatch(action);
-    console.log('%ccurrent state: ', 'color: green', store.getState());
-    console.groupEnd();
-    return value;
-  };
+      //i had an error using store.dispatch instead of rawDispatch, which
+      //calls the wrapper recursively
+      const value = next(action);
+      console.log('%ccurrent state: ', 'color: green', store.getState());
+      console.groupEnd();
+      return value;
+    };
 };
 
-const addPromiseSupportToDispatch = store => {
-  const rawDispatch = store.dispatch;
+const promise = store => next => action => {
+      if (typeof action.then === 'function') {
+        return action.then(a => next(a));
+      } else {
+        return next(action);
+      }
+    };
 
-  return action => {
-    if (typeof action.then === 'function') {
-      return action.then(a => rawDispatch(a));
-    } else {
-      return rawDispatch(action);
-    }
-  };
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+    let next = store.dispatch;
+    middlewares.slice().reverse()
+    .forEach(middleware => next = middleware(store)(next));
+    store.dispatch = next;
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  const middlewares = [promise];
 
   //it's like a stack
   //log is added first and it's at the bottom.
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  //promise is added second and it's at the top.
-  store.dispatch = addPromiseSupportToDispatch(store);
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   store.subscribe(throttle(() => {
   }, 1000));
